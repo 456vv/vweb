475bd9e85076de70acce4af93190faa0f8caa218
456vv <123@456vv.com>
2020-07-04 18:37:59 +0800

    no message

--- vweb.go
+++ vweb.go
@@ -7,7 +7,7 @@ import(
 
 //其它
 const (
-    Version                 string = "VWEB/v2.0.0"                     					// 版本号
+    Version                 string = "VWEB/v2.0.x"                     					// 版本号
     defaultDataBufioSize    int64 = 32*1024           									// 默认数据缓冲32MB
 )
 
