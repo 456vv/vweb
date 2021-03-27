48d636cdd311a286e201e8f71c3ed75a68a7df02
456vv <123@456vv.com>
2021-03-27 12:55:43 +0800

    no message

--- server/cmd/main/all_build.bat
+++ server/cmd/main/all_build.bat
@@ -1,9 +1,5 @@
 go mod tidy
 
-set GOOS=windows
-set GOARCH=amd64
-go build -o bin/V-WEB-Server-win-amd64.exe -ldflags="-s -w" ./
-
 set GOOS=linux
 set GOARCH=386
 go build -o bin/V-WEB-Server-linux-386 -ldflags="-s -w" ./
@@ -16,6 +12,14 @@ set GOARCH=arm64
 go build -o bin/V-WEB-Server-linux-arm64 -ldflags="-s -w" ./
 set GOARCH=mips
 go build -o bin/V-WEB-Server-linux-mips -ldflags="-s -w" ./
+
+
+set GOOS=windows
+set GOARCH=amd64
+go build -o bin/V-WEB-Server-win-amd64.exe -ldflags="-s -w" ./
+
+set CGO_ENABLED=1
+go build -o bin/V-WEB-Server-win-amd64-sass.exe --tags libsass  -ldflags="-s -w" ./
 
 upx -9 ./bin/*
 
