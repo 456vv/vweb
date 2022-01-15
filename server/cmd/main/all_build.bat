723ea1980bf72ec33bd23ffce1494f1b4104ddfa
456vv <123@456vv.com>
2022-01-16 00:22:48 +0800

    no message

--- server/cmd/main/all_build.bat
+++ server/cmd/main/all_build.bat
@@ -1,28 +1,34 @@
 go mod tidy
 go mod download
 
-set version=App/%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+set /p tags=CGO_ENABLED=0/go:build tags:
+
+set version=App/%date:~0,4%%date:~5,2%%date:~8,2%
 set CGO_ENABLED=0
 set GOOS=linux
 set GOARCH=386
-go build -o bin/V-WEB-Server-linux-386 -ldflags="-s -w -X main.version=%version%" ./
+go build -o bin/V-WEB-Server-linux-386 -ldflags="-s -w -X main.version=%version%" -tags="%tags%"  ./
 set GOARCH=amd64
-go build -o bin/V-WEB-Server-linux-amd64 -ldflags="-s -w -X main.version=%version%" ./
+go build -o bin/V-WEB-Server-linux-amd64 -ldflags="-s -w -X main.version=%version%" -tags="%tags%" ./
 set GOARCH=arm
 set GOARM=7
-go build -o bin/V-WEB-Server-linux-armv7 -ldflags="-s -w -X main.version=%version%" ./
+go build -o bin/V-WEB-Server-linux-armv7 -ldflags="-s -w -X main.version=%version%" -tags="%tags%" ./
 set GOARCH=arm64
-go build -o bin/V-WEB-Server-linux-arm64 -ldflags="-s -w -X main.version=%version%" ./
+go build -o bin/V-WEB-Server-linux-arm64 -ldflags="-s -w -X main.version=%version%" -tags="%tags%" ./
 set GOARCH=mips
-go build -o bin/V-WEB-Server-linux-mips -ldflags="-s -w -X main.version=%version%" ./
+go build -o bin/V-WEB-Server-linux-mips -ldflags="-s -w -X main.version=%version%" -tags="%tags%" ./
 
 
 set GOOS=windows
 set GOARCH=amd64
 go build -o bin/V-WEB-Server-win-amd64.exe -ldflags="-s -w -X main.version=%version%" ./
 
-::set CGO_ENABLED=1
-::go build -o bin/V-WEB-Server-win-amd64-general.exe -ldflags="-s -w  -X main.version=$version -extldflags '-static -fpic'" -tags "general" ./
+set /p tags=CGO_ENABLED=1/go:build tags:
+if "%tags%" == "" goto upx
+set CGO_ENABLED=1
+go build -o bin/V-WEB-Server-win-amd64-general.exe -ldflags="-s -w  -X main.version=$version -extldflags '-static -fpic'"  -tags="%tags%" ./
+
+:upx
 
 upx -9 ./bin/*
 
