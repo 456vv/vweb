a5dcefb587941f6ff013a1d4be1d550c01742abc
456vv <123@456vv.com>
2022-01-18 11:02:50 +0800

    no message

--- server/cmd/main/debug.bat
+++ server/cmd/main/debug.bat
@@ -3,13 +3,13 @@ taskkill /T /F /IM V-WEB-Server-win-amd64.exe
 go mod tidy
 go mod download
 
-set version=App/%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+set version=App/%date:~0,4%%date:~5,2%%date:~8,2%
 set GOOS=windows
 set GOARCH=amd64
-go build -gcflags "-N -l" -ldflags="-X main.version=%version%" -o bin/V-WEB-Server-win-amd64.exe
+go build -gcflags "-N -l" -ldflags="-X main.version=%version%" -tags="vweb_lib yaegi_lib" -o bin/V-WEB-Server-win-amd64.exe
 
 set PATH=K:\code\GO\bin;%PATH%
 cd /D bin
-V-WEB-Server-win-amd64.exe -RootDir ../test -ConfigFile config.json
+gdlv exec V-WEB-Server-win-amd64.exe -RootDir ../test -ConfigFile config.json
 
 pause
